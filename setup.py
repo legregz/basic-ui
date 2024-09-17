91a19fdf1902972581d2fcdcfba3c93cad30edaf
legregz <gregggmar01@gmail.com>
2024-09-18 00:55:45 +0200

    update for first upload in pypi repos

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pygui',
+    version='0.1',
+    packages=find_packages(),
+    install_require=[
+        'pygame>=2.6.0'
+    ],
+)
