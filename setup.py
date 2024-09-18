c628f4d6401afc88a1a3128210a7b34160804f20
legregz <gregggmar01@gmail.com>
2024-09-18 02:32:01 +0200

    🎉 starting project basic_ui

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='basic-ui',
-    version='0.1',
+    name='basic_ui',
+    version='0.3',
     packages=find_packages(),
     install_require=[
         'pygame>=2.6.0'
