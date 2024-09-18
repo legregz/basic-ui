6fec9e1fd1def1af6a55960becacd05d883d3b31
legregz <gregggmar01@gmail.com>
2024-09-18 17:34:47 +0200

    ✅ final update of python-publish.yml file passed test with success

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='basic_ui',
-    version='0.4.4',
+    version='0.4.5',
     packages=find_packages(),
     install_require=[
         'pygame>=2.6.0'
