93991c4888786a0055190260a5530740b112fb8f
legregz <gregggmar01@gmail.com>
2024-09-18 17:18:29 +0200

    🐛 fix setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         'pygame>=2.6.0'
     ],
     author='Legregz',
-    description="basic ui is a package for pygame to create interfaces easly based on json files"
+    description="basic ui is a package for pygame to create interfaces easly based on json files",
     long_description_content_type='text/markdown',
     long_description=open('README.md').read()
 )
